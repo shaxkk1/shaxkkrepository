39aa7156290ac889f210e1f8c59ab4275b7ed8cb
shaxkk1 <pigstep95@gmail.com>
2024-11-06 16:49:56 -0500

    Today on worked on python variables and if statements

---
+++ python_1/shaxkk python project.py
@@ -0,0 +1,40 @@
+# variables
+'''
+Variables are a way for your program to remember information
+Whether it is a string, an integer, or a float
+Make sure variable names are descriptive, so that the program is easy to code
+'''
+# Snake Case
+first_name = "Sean" #This is a string
+
+#Camel case
+#Camel case because the variable name looks like the hump of a camel.
+lastName = "Dao" # This is also a string
+nickName = "Sugar Sean" # This is also a string 
+
+age = 16 # This is an int (integer)
+bankAccountBalance = 18346.34 # This is a float
+
+print( first_name )
+print( bankAccountBalance )
+
+# Command k, command t to scroll through different themes 
+
+# if statements
+if bankAccountBalance > 100: #Only time this will run is if bankAccountBalance is over 100
+    print("Sugar Sean with the fireman's carry. Watch your back!!") # This is a string
+
+if age >= 18:
+    print("You are eligable to vote!")
+else:
+    print("You can't vote yet buddy. Sorry.")
+# Command forward slash to make code a comment
+
+# Simple mini application for applesauce program
+
+print("Welcome to the APPLE SAUCE PROGRAM")
+
+userAppleSauceCount = input ("How many apple sauces would you like: ")
+
+print( "The user said", userAppleSauceCount)
+
